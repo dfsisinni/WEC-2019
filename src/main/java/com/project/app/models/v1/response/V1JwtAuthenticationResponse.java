76cbb89911eded85c6b8d97661829b7e105385d4
Dane Sisinni <dfsisinni@gmail.com>
2019-01-11 16:05:38 -0500

    sample authentication for spring with jwt tokens and mysql

---
+++ src/main/java/com/project/app/models/v1/response/V1JwtAuthenticationResponse.java
@@ -0,0 +1,16 @@
+package com.project.app.models.v1.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class V1JwtAuthenticationResponse {
+
+    private String token;
+
+}
---
+++ src/main/resources/liquibase/changesets/create_user.sql
@@ -0,0 +1,4 @@
+create table user (
+  username VARCHAR(120) PRIMARY KEY,
+  password VARCHAR(120) NOT NULL
+);
