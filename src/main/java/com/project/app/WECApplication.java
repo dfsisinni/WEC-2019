591682383b4e9f39cdf35505327d9676fdce2c3d
Dane Sisinni <dfsisinni@gmail.com>
2019-01-11 16:18:34 -0500

    rename main application file

--- src/main/java/com/project/app/WECApplication.java
+++ src/main/java/com/project/app/WECApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthApplication {
+public class WECApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthApplication.class, args);
+        SpringApplication.run(WECApplication.class, args);
     }
 
 }
